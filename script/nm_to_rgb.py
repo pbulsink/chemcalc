eb8a7b41d8867ee2cd83ae80f98181acf8b180b6
Phil OSx <philip.bulsink@uottawa.ca>
2014-01-18 19:44:30 -0500

    Removed dependance on future for python's division, and used float to force non-integer results

--- script/nm_to_rgb.py
+++ script/nm_to_rgb.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from __future__ import division
 
 def wavelength_to_rgb(wavelength):
     """
@@ -8,32 +7,32 @@ def wavelength_to_rgb(wavelength):
     Based on http://www.physics.sfasu.edu/astro/color/spectra.html
     """
 
-    red = 0.0
-    green = 0.0
-    blue = 0.0
+    red = 0
+    green = 0
+    blue = 0
     intensity = 1
     
     if wavelength < 380:
         pass
     elif wavelength < 440:
-        red = -(wavelength-440)/(440-380)
+        red = -(wavelength-440)/float(440-380)
         green = 0
         blue = 1
     elif wavelength < 490:
         red = 0
-        green = (wavelength - 440)/(490-440)
+        green = (wavelength - 440)/float(490-440)
         blue = 1
     elif wavelength < 510:
         red = 0
         green = 1
-        blue = -(wavelength - 510)/(510-490)
+        blue = -(wavelength - 510)/float(510-490)
     elif wavelength < 580:
         red = (wavelength-510)/(580-510)
         green = 1
         blue = 0
     elif wavelength < 645:
         red = 1
-        green = -(wavelength-645)/(645-580)
+        green = -(wavelength-645)/float(645-580)
         blue = 0
     elif wavelength < 780:
         red = 1
@@ -43,9 +42,9 @@ def wavelength_to_rgb(wavelength):
         pass
 
     if wavelength > 380 and wavelength < 420:
-        intensity = 0.3 + 0.7*(wavelength - 380)/(420-380)
+        intensity = 0.3 + 0.7*(wavelength - 380)/float(420-380)
     elif wavelength > 700 and wavelength <= 780:
-        intensity = 0.3 + 0.7*(780 - wavelength)/(780-700)
+        intensity = 0.3 + 0.7*(780 - wavelength)/float(780-700)
     
     red = int(red * 255 * intensity)
     green = int(green * 255 * intensity)
